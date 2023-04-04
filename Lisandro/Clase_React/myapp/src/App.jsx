b9bb02c4becb97d74b4f19f82f83079c12f71eca
Valeria Sanabria <valerisanabria@tdf.edu.ar>
2023-04-04 19:09:42 -0300

    footer

--- Lisandro/Clase_React/myapp/src/App.jsx
+++ Lisandro/Clase_React/myapp/src/App.jsx
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/header/header.jsx';
+import Content from './componentes/content/content.jsx';
+import Footer from './componentes/footer/footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <p>Hola mathi</p>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
