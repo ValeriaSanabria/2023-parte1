6e4aa7a078dba941cc59858bcf8f181ce1ec4060
Valeria Sanabria <valerisanabria@tdf.edu.ar>
2023-04-11 20:56:59 -0300

    hooks

--- Lisandro/Clase_React/myapp/src/ejerciciosClase/useState.jsx
+++ Lisandro/Clase_React/myapp/src/ejerciciosClase/useState.jsx
@@ -1,9 +1,23 @@
-import React , {useState} from "react";
+import React , {useState, useEffect} from "react";
+import { useEffect } from "react";
 
 const Ejercicio = () => {
 
+    // desestructuacion de react de arrays
+    // con setPeso modificamos a peso mediante setPeso(peso)
+    const[peso, setPeso] = useState(0);
 
-    
+
+    useEffect(
+        ()=>{
+            fetchUsers()
+// el parametro [] siempre tiene que estar vacio para que no se vuelva a repetir el uso de la api
+        },[]     
+    )
+
+    return(
+        <h2>Hola mundo</h2>
+    )
 
 
 
